c708f1d6f172ca6e12ad6f4f6944a896110a043b
Trey Brisbane <trey@treybrisbane.com>
2014-05-09 09:11:29 +1000

    Added session handling for login actions.

--- app/controllers/AuthenticationController.java
+++ app/controllers/AuthenticationController.java
@@ -11,16 +11,11 @@ import play.mvc.Result;
  */
 public class AuthenticationController extends Controller {
 	/**
-	 * Login form to be presented to the user when they request to login to the FlightPub system.
-	 */
-	final static Form<User> loginForm = Form.form(User.class, User.LoginFields.class);
-
-	/**
 	 * Login action - displays the FlightPub login form.
 	 * @return The Login form.
 	 */
 	public static Result login() {
-		return ok(views.html.login.render(loginForm));
+		return ok(views.html.login.render(Form.form(User.class, User.LoginFields.class)));
 	}
 
 	/**
@@ -30,8 +25,14 @@ public class AuthenticationController extends Controller {
 	 * failure.
 	 */
 	public static Result processLogin() {
-		//@TODO: Process login
-		return null;
+		Form<User> loginForm = Form.form(User.class, User.LoginFields.class).bindFromRequest();
+		if (loginForm.hasErrors()) {
+			return badRequest(views.html.login.render(loginForm));
+		} else {
+			session().clear();
+			session("email", loginForm.get().email);
+			return redirect(controllers.routes.MainController.home());
+		}
 	}
 
 	/**
@@ -39,7 +40,7 @@ public class AuthenticationController extends Controller {
 	 * @return A redirect to the FlightPub homepage.
 	 */
 	public static Result logout() {
-		//@TODO: Invalidate session
-		return redirect("/");
+		session().clear();
+		return redirect(controllers.routes.MainController.home());
 	}
 }
