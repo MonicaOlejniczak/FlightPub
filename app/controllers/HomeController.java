8b82aa336c7f96d504d24878dd5d1d1df1fc7902
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2014-05-14 12:27:31 +1000

    fixed routes and broken things

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -4,7 +4,7 @@ import models.Flight;
 import play.mvc.Controller;
 import play.mvc.Result;
 
-import java.util.List;
+import java.util.*;
 
 public class HomeController extends Controller {
 
@@ -14,7 +14,15 @@ public class HomeController extends Controller {
 	}
 
 	public static Result processFlights() {
-		List<Flight> flights = Flight.find.setMaxRows(10).findList();
+		// this is stupid </rage>
+		/*RawSql rawSql = RawSqlBuilder
+						.parse("SELECT * FROM Flight GROUP BY flightNumber")
+						.columnMapping("flightNumber", "flightNumber")
+						.create();
+		List<Flight> flights = Flight.find.setRawSql(rawSql).findList();*/
+		List<Integer> idList = new LinkedList<>();
+		idList.addAll(Arrays.asList(1, 446, 872, 1311, 1752, 2193, 2626, 3065, 3941, 4380, 4806, 5243, 5668, 6110, 6552, 6994, 7417));
+		List<Flight> flights = Flight.find.select("flightNumber").where().idIn(idList).findList();
 		return ok(views.html.flights.render(flights));
 	}
 
