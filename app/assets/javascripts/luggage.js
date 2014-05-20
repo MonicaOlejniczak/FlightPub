dc873e2ce2df30f70cc3068929a7349b0b7517ae
Christopher Emery <emery.christopherjohn@gmail.com>
2014-05-20 18:15:33 +1000

    Further streamlined luggage.

--- app/assets/javascripts/luggage.js
+++ app/assets/javascripts/luggage.js
@@ -1,4 +1,4 @@
-function confirmCancel(){
+function confirmCan(){
     var answer = confirm("Are you sure you wish to cancel your current booking?\nClick OK to confirm or CANCEL to continue with your booking.");
     if(answer){
         window.location = "/";
