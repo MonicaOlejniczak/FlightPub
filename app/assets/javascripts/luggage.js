2f23b7f7a469e3d688fea213136b1b6c9c1332a8
Christopher Emery <emery.christopherjohn@gmail.com>
2014-05-22 08:00:09 +1000

    Added ticket class option into luggage page. This page now serves as both the luggage selection and ticket information page. Also improved format of the form.

--- app/assets/javascripts/luggage.js
+++ app/assets/javascripts/luggage.js
@@ -1,17 +1,21 @@
 window.onload=function generateTable(){
-    var messageh = '<p class="centre-block">\nFor each passenger, please select whether you wish to include only carry-on luggage with your booking, or if you wish to also include checked luggage.\n</p>\n';
+    var messageh = '<p class="centre-block">\nFor each passenger, please select what class of ticket and '
+                   + 'whether you wish to include only carry-on luggage with your booking, or if you wish to also include checked luggage.\n</p>\n';
     var formh = '<FORM class="form" role="form" method="POST" action="/luggage">\n';
-    var formb = '';
-    for(var i = 0; i < 4; i++){
-        formb += '<p>Passenger ' + (i + 1) + ':</p>\n';
-        formb += '<div class="radio">\n<label>\n<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>\n';
-        formb += 'Only Carry-On Luggage\n</label>\n</div>';
-        formb += '<div class="radio">\n<label>\n<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">\n';
-        formb += 'Carry-On Plus Checked Baggage\n</label>\n</div>';
+    var formb = '<div>\n';
+    for(var j = 0; j < 4; j++){
+        formb += '<p>Passenger ' + (j + 1) + ':</p>\n';
+        formb += '<select class="form-control">\n<option>First Class</option>\n';
+        formb += '<option>Business Class</option>\n';
+        formb += '<option>Premium Economy</option>\n';
+        formb += '<option>Economy</option>\n</select>\n';
+        formb += '<select class="form-control">\n<option>Only Carry-On Luggage</option>\n';
+        formb += '<option>Carry-On Plus Checked Baggage</option>\n</select>\n';
     }
-    var formf = '<a href="/seatSelection" class="btn btn-primary">Previous</a>';
+    formb += '</div>\n';
+    var formf = '</div>\n<a href="/seatSelection" class="btn btn-primary">Previous</a>';
     formf += '<input type="submit" class="btn btn-primary" value="Next">';
-    formf += '<button class="btn btn-danger" onclick="confirmCan();">Cancel</button>\n';
+    formf += '<button class="btn btn-danger" onclick="confirmCan();">Cancel</button>\n</div>\n';
     formf += '</FORM>\n';
     var page = messageh + formh + formb + formf;
     document.getElementById('luggage').innerHTML = page;
