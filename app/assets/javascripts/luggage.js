9e5c906a5b9d76c456c5c83073792d66de5415fc
Christopher Emery <emery.christopherjohn@gmail.com>
2014-05-26 07:55:49 +1000

    Fixed formatting on the luggage/ticket selection page.

--- app/assets/javascripts/luggage.js
+++ app/assets/javascripts/luggage.js
@@ -1,18 +1,20 @@
 window.onload=function generateTable(){
     var messageh = '<p class="centre-block">\nFor each passenger, please select what class of ticket and '
                    + 'whether you wish to include only carry-on luggage with your booking, or if you wish to also include checked luggage.\n</p>\n';
-    var formh = '<FORM class="form" role="form" method="POST" action="/luggage">\n';
-    var formb = '<div>\n';
+    var formh = '<FORM class="form-group" role="form" method="POST" action="/luggage">\n';
+    var formb = '';
     for(var j = 0; j < 4; j++){
+        formb += '<div>\n';
         formb += '<p>Passenger ' + (j + 1) + ':</p>\n';
-        formb += '<select class="form-control">\n<option>First Class</option>\n';
-        formb += '<option>Business Class</option>\n';
+        formb += '<div>\n';
+        formb += '<select class="form-control">\n<option>Economy</option>\n';
         formb += '<option>Premium Economy</option>\n';
-        formb += '<option>Economy</option>\n</select>\n';
+        formb += '<option>Business Class</option>\n';
+        formb += '<option>First Class</option>\n</select>\n';
         formb += '<select class="form-control">\n<option>Only Carry-On Luggage</option>\n';
         formb += '<option>Carry-On Plus Checked Baggage</option>\n</select>\n';
+        formb += '</div>\n<br /></div>\n';
     }
-    formb += '</div>\n';
     var formf = '</div>\n<a href="/seatSelection" class="btn btn-primary">Previous</a>';
     formf += '<input type="submit" class="btn btn-primary" value="Next">';
     formf += '<button class="btn btn-danger" onclick="confirmCan();">Cancel</button>\n</div>\n';
