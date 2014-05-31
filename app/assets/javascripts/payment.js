97be030bf782a953501b8f63625447e2e5e58285
Christopher Emery <emery.christopherjohn@gmail.com>
2014-05-31 17:53:49 +1000

    Fixed styling on about page. Added in pay method options. Now working on displaying form fields for which method has been selected.

--- app/assets/javascripts/payment.js
+++ app/assets/javascripts/payment.js
@@ -1,6 +1,6 @@
-function confirmCancel(){
+function confirmCancel() {
     var answer = confirm("Are you sure you wish to cancel your current booking?\nClick OK to confirm or CANCEL to continue with your booking.");
-    if(answer){
+    if (answer) {
         window.location = "/";
     }
 }
