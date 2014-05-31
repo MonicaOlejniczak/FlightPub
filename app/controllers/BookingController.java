db51c925fd2d3e311becc12d49af103620395356
Christopher Emery <emery.christopherjohn@gmail.com>
2014-05-31 13:20:16 +1000

    Set up payment options page. Currently blank, will begin to add options to the form.

--- app/controllers/BookingController.java
+++ app/controllers/BookingController.java
@@ -2,6 +2,9 @@ package controllers;
 
 import models.Luggage;
 import models.Seat;
+import models.User;
+import play.data.Form;
+import play.data.validation.Constraints;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -18,4 +21,56 @@ public class BookingController extends Controller {
         List<Luggage> luggageTypes = Luggage.find.all();
         return ok(views.html.luggage.render(luggageTypes));
     }
+
+    /**
+     * Inner static class to specify and validate the fields used in the payment form.
+     */
+    public static class PaymentForm {
+        /**
+         * First Name.
+         */
+        @Constraints.Required(message = "Required Field!")
+        public String nameFirst;
+
+        /**
+         * Surname.
+         */
+        @Constraints.Required(message = "Required Field!")
+        public String surname;
+
+        /**
+         * Street Address.
+         */
+        @Constraints.Required(message = "Required Field!")
+        public String streetAddress;
+
+        /**
+         * Suburb/City.
+         */
+        @Constraints.Required(message = "Required Field!")
+        public String suburbCity;
+
+        /**
+         * State.
+         */
+        @Constraints.Required(message = "Required Field!")
+        public String stateIn;
+
+        /**
+         * Post code.
+         */
+        @Constraints.Required(message = "Required Field!")
+        public String postCode;
+
+        /**
+         * Phone Number.
+         */
+        @Constraints.Required(message = "Required Field!")
+        public String phoneNumber;
+    }
+
+    public static Result payment() { return ok(views.html.payment.render(Form.form(PaymentForm.class))); }
+
+    public static Result submitPayment(String destination) { return ok (views.html.home.render()); }
+
 }
