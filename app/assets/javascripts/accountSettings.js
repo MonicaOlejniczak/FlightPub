2920674ee38da99823d423f7b2aa866cadf5102e
Christopher Emery <emery.christopherjohn@gmail.com>
2014-06-02 00:05:26 +1000

    These files are related to the previous commit. Must have forgotten to stage them. Derp.

--- app/assets/javascripts/accountSettings.js
+++ app/assets/javascripts/accountSettings.js
@@ -1,3 +1,19 @@
-/**
- * Created by Christopher on 1/06/2014.
- */
+function confirmPassword() {
+    if(document.getElementById("accPassword").value != document.getElementById("accConfPassword").value) {
+        alert("Password confirmation failed! Re-enter your password!");
+        return false;
+    } else {
+        document.getElementById("accForm").submit();
+        return true;
+    }
+}
+
+function confirmCancel(){
+    var answer = confirm("Are you sure you wish to cancel your current booking?\nClick OK to confirm or CANCEL to continue with your booking.");
+    if(answer){
+        document.getElementById("accForm").reset();
+        return true;
+    } else {
+        return false;
+    }
+}
--- app/assets/javascripts/register.js
+++ app/assets/javascripts/register.js
@@ -1,3 +1,19 @@
-/**
- * Created by Christopher on 1/06/2014.
- */
+function confirmRegPassword() {
+    if(document.getElementById("password").value != document.getElementById("confirmPassword").value) {
+        alert("Password confirmation failed! Re-enter your password!");
+        return false;
+    } else {
+        document.getElementById("regForm").submit();
+        return true;
+    }
+}
+
+function confirmCancel(){
+    var answer = confirm("Are you sure you wish to cancel your current booking?\nClick OK to confirm or CANCEL to continue with your booking.");
+    if(answer){
+        document.getElementById("regForm").reset();
+        return true;
+    } else {
+        return false;
+    }
+}
