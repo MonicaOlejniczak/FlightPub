2fa3184839ec480d755a9136cd97a11b3eddbbb7
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2014-09-17 19:49:33 +1000

    Fixed the booking display.

--- public/app/view/account/booking/BookingController.js
+++ public/app/view/account/booking/BookingController.js
@@ -13,15 +13,18 @@ Ext.define('FB.view.account.booking.BookingController', {
         var destination = itinerary.flights[itinerary.flights.length - 1].destination;
         var departureTime = new Date(itinerary.departureTime);
         var arrivalTime = new Date(itinerary.arrivalTime);
-        var dateFormat = 'l \\t\\he jS \\of F, Y \\a\\t H:i:s A';
+        var dateFormat = 'l \\t\\he jS \\of F, Y';
+        var timeFormat = 'H:i:s A';
         view.update({
             source: source.name,
             destination: destination.name,
             status: status,
-            arrivalTime: Ext.Date.format(arrivalTime, dateFormat),
-            departureTime: Ext.Date.format(departureTime, dateFormat)
+            departureDate: Ext.Date.format(departureTime, dateFormat),
+            departureTime: Ext.Date.format(departureTime, timeFormat),
+            arrivalDate: Ext.Date.format(arrivalTime, dateFormat),
+            arrivalTime: Ext.Date.format(arrivalTime, timeFormat),
         });
-        var title = source.name + " to " + destination.name;
+        var title = Ext.String.format("{0} to {1}", source.name ,destination.name);
         view.setTitle(title);
     }
 });
