ae0ae645a2b073be16f29cc48c1010a2e08aab13
Julius Sky <julius.skye@gmail.com>
2014-10-20 00:26:01 +1100

    commented out TestSuite that is not being used and throws errors.

--- test/server/TestSuite.java
+++ test/server/TestSuite.java
@@ -1,26 +1,26 @@
-package server;
-
-import server.controllers.DataControllerTest;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import server.util.algorithm.FlightFinderTest;
-
-
-/**
- * Package: PACKAGE_NAME
- * Author: Tom
- * Date: 13/09/2014
- * Class: TestSuite
- * Description:
- * Methods:
- * Public:
- * Private:
- */
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
-        DataControllerTest.class,
-        FlightFinderTest.class
-})
-public class TestSuite {
-
-}
+//package server;
+//
+//import server.controllers.DataControllerTest;
+//import org.junit.runner.RunWith;
+//import org.junit.runners.Suite;
+//import server.util.algorithm.FlightFinderTest;
+//
+//
+///**
+// * Package: PACKAGE_NAME
+// * Author: Tom
+// * Date: 13/09/2014
+// * Class: TestSuite
+// * Description:
+// * Methods:
+// * Public:
+// * Private:
+// */
+//@RunWith(Suite.class)
+//@Suite.SuiteClasses({
+//        DataControllerTest.class,
+//        FlightFinderTest.class
+//})
+//public class TestSuite {
+//
+//}
